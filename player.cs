69ae19c6ee420e73a5fdaec8a773d7680e9ec678
Blas Cerecer <blcerecer@gmail.com>
2019-02-13 20:44:37 -0800

    gets user input; flip character correctly

--- player.cs
+++ player.cs
@@ -5,45 +5,48 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     public Rigidbody2D rigidBod;
-    public float runSpeed;
+    public float runSpeed;                                              //runSpeed is the speed set by user
+    float velocity;                                                             //velocity is used to change direction of player using runSpeed
     float accelerationClick;
     public float tiredSpeed;
-    public bool lookright = true;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBod = GetComponent<Rigidbody2D>();
-        accelerationClick = runSpeed;
+        velocity = -runSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-
         Debug.Log(transform.position.x);
         Debug.Log(rigidBod.velocity);
 
+        accelerationClick = velocity * Input.GetAxis("Horizontal");
+    }
 
-        if (transform.position.x < -2)
-        {
-            accelerationClick = runSpeed;
-            Flip();
-        }
-        else if (transform.position.x > 2)
-        {
-            accelerationClick = -runSpeed;
-            Flip();
-        }
-
-        rigidBod.velocity = new Vector2(accelerationClick, rigidBod.velocity.y);
+    void FixedUpdate()
+    {
+        rigidBod.velocity = new Vector2(velocity + accelerationClick, rigidBod.velocity.y); 
+        Flip();
     }
 
     void Flip()
     {
-        lookright = !lookright;
-        Vector3 charscale = transform.localScale;
-        charscale.x *= -1;
-        transform.localScale = charscale;
+        if (transform.position.x < -2)
+        {
+            Vector3 charscale = transform.localScale;
+            charscale.x *= -1;                                                  //multiplies value of Vector3.x by -1 to change direction
+            transform.localScale = charscale;
+            velocity = runSpeed;
+        }
+        else if (transform.position.x > 2)
+        {
+            Vector3 charscale = transform.localScale;
+            charscale.x *= -1;
+            transform.localScale = charscale;
+            velocity = -runSpeed;
+        }
     }
 }
