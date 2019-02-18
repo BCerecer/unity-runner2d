79327c5661dbb19d7f0ec612d2cbf5cc08297e95
Blas Cerecer <blcerecer@gmail.com>
2019-02-18 12:24:17 -0800

    fixed bug that didn't turn the player on the horizontal axis to face where he was running

--- player.cs
+++ player.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     public Rigidbody2D rigidBod;
-    public float runSpeed;                                              //runSpeed is the speed set by user
-    float velocity;                                                             //velocity is used to change direction of player using runSpeed
+    public float runSpeed;                                                  //runSpeed is the speed set by user
+    float velocity;                                                         //velocity is used to change direction of player using runSpeed
     float accelerationClick;
     public float tiredSpeed;
 
@@ -20,10 +20,10 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(transform.position.x);
-        Debug.Log(rigidBod.velocity);
+        //Debug.Log(transform.position.x);
+        //Debug.Log("Velocity " + rigidBod.velocity);
+                   
 
-        accelerationClick = velocity * Input.GetAxis("Horizontal");
     }
 
     void FixedUpdate()
@@ -34,10 +34,12 @@ public class player : MonoBehaviour
 
     void Flip()
     {
+        //Debug.Log("localscale.x = " + transform.localScale.x);
+
         if (transform.position.x < -2)
         {
             Vector3 charscale = transform.localScale;
-            charscale.x *= -1;                                                  //multiplies value of Vector3.x by -1 to change direction
+            charscale.x *= -1;                                              //multiplies value of Vector3.x by -1 to change direction
             transform.localScale = charscale;
             velocity = runSpeed;
         }
@@ -47,6 +49,14 @@ public class player : MonoBehaviour
             charscale.x *= -1;
             transform.localScale = charscale;
             velocity = -runSpeed;
+        }
+
+        if (transform.localScale.x == (velocity / runSpeed * 2) )           //If stil going backwards, fix it
+        {
+            Debug.Log("IT IS GOING BACKWARDSSSSSSSSSSSSS");
+            Vector3 charscale = transform.localScale;
+            charscale.x *= -1;
+            transform.localScale = charscale;
         }
     }
 }
